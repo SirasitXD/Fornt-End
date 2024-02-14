56e5f71c3fa5d23cab841197920dfebea08018b3
SirasitXD <modkungza2@gmail.com>
2024-02-14 11:01:27 +0700

    a

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -10,7 +10,7 @@ app.set("views", path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.set(express.static(__dirname, "/public"));
+// app.set(express.static(__dirname, "/public"));
 
 app.get("/", async (req, res) => {
     try {
