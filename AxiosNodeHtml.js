b76ff836412b374ee3b3139fa3f6728282405b8c
SirasitXD <modkungza2@gmail.com>
2024-02-14 10:17:01 +0700

    a

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const axios = require("axios");
+
 const app = express();
 var bodyParser = require("body-parser");
 const base_url = "http://node56967-env-0063028.proen.app.ruk-com.cloud";
+
+
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
